5dfa026a15874fdaec706b0d7d330ef211636d2f
swesadiqul <mdsadiqulislam390@gmail.com>
2023-12-31 15:22:47 +0600

    register complete

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            
+            # Log the user in after signup (optional)
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            
+            # Redirect to a success page or home
+            return redirect('home')
+    return render(request, 'accounts/signup.html')
--- event_registration_system/settings.py
+++ event_registration_system/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'event.apps.EventConfig',
-    'accounts.apps.AccountConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
